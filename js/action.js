0cbe1b9f127e6e2b91fe2d65923f04921b85117a
ahmedalaa404 <az.ahmed.alaa@gmail.com>
2022-09-20 05:24:34 +0200

    make function of regualr experession

--- js/action.js
+++ js/action.js
@@ -1,4 +1,6 @@
 // get inputs of html 
+
+"use strict"
 let ProductName=document.getElementById("ProductName");
 let ProductCatgorey=document.getElementById("ProductCatgorey");
 let ProductPrice=document.getElementById("ProductPrice");
@@ -7,40 +9,49 @@ let SearchProduct=document.getElementById("SearchProduct");
 let removeAll=document.getElementById('remove');
 let counter = document.getElementById('counter');
 let searchInputs=document.getElementById('searchInputs');
+
+// random sentences appearing
+let quoteWords=document.getElementById("quoteWords");
+let nameSpeaker=document.getElementById("nameSpeaker");
+
 // get button of html 
 let btnAdd=document.getElementById("btnAdd");
 let btnUpdate=document.getElementById("btnUpdate");
-let btnDelete=document.getElementById("btnDelete");
+let btnClear=document.getElementById("btnClear");
 let cansle=document.getElementById("cansle");
 let deletData=document.getElementById("deletData");
-let quoteWord=document.getElementById("quoteWords");
-let nameSpeaker=document.getElementById("nameSpeaker");
+
+//  event static
+cansle.addEventListener("click",showDiv);
+deletData.addEventListener("clcik",remove);
+
 // get tbody of table to show  a data 
 let tbody=document.getElementById("tbody");
+
+
 // variable used in js to store Data 
 let store;
+
 let concat=``;
-DateOFDay=new Date();
+
+ let DateOFDay=new Date();
+
 let numberchangeUpdate;
+
+// random sentences appearing
+
 let quots=["Whatever it is, a person does not lose in any circumstance what God has bestowed upon him of honor.",
 "Had it not been for its tides, and if it were not for its magic, the owner of his heart would not wish for Sidon, seek refuge in your heart from the arrows of its sight.. Or die as love willed as a martyr, if you saw beauty and did not matter.. You were a coarse, dull person.",
 "Blessed is the love in which there is no owner and the other is owned.. Both are loyal to the other."];
 let speakers=["- Malik bin Nabi","- Elia Abu Madi", "- Jorge Luis Borges"];
 let rounded=parseInt(Math.random()*3);
-//  variable reguxe
-let reguxeName=/^[A-Z]/;
 
-// function tesst()
-// {
-// if(reguxeName.test("123")==true)
-// {
-//     return 321;
-// }
-// else
-// {
-//     return 123;
-// }
-// }
+
+//  variable reguxe
+let reguxeName=/^[a-zA-Z1-9 ]{3,20}$/;
+let reguxNumber=/^[1-9][0-9]{1,12}/;
+let reguxCatgorey=/^[a-z]{2,10}$/;
+let reguxDes=/^[a-z]{2,40}[a-zA-Z1-9 ,]{2,140}$/;
 
 // Function to retrieve Data of Local storage and make a quote rounded 
 (function()
@@ -58,17 +69,62 @@ let reguxeName=/^[A-Z]/;
     displayData(store);
 })()
 
-// reguxe
+// reguxe Fucntion
+function validation(reg,input)
+{
+    if(reg.test(input.value)==true)
+    {
+        input.classList.replace("is-invalid","is-valid");
+        return true;
+    }
+    else
+    {
+        if(input.classList.contains("is-valid"))
+        {
+            input.classList.replace("is-valid","is-invalid");
+        }
+        return false;
+    }
+}
+function callreguex()
+{
+    validation(reguxNumber,ProductPrice);
+    validation(reguxeName,ProductName);
+    validation(reguxDes,ProductDesc);
+    validation(reguxCatgorey,ProductCatgorey);
+}
+// events of reguex 
+
+ProductName.addEventListener("input",function()
+{
+    validation(reguxeName,ProductName);
+})
+
+ProductPrice.addEventListener("input",function()
+{
+    validation(reguxNumber,ProductPrice);
+})
+
+ProductDesc.addEventListener("input",function()
+{
+    validation(reguxDes,ProductDesc);
+})
+
+ProductCatgorey.addEventListener("input",function()
+{
+    validation(reguxCatgorey,ProductCatgorey);
+})
 
 
+//  done of regux ;
 
 
-
-// function remove Data in local storage
+// function remove all  Data in local storage
 function clearData()
 {
 document.getElementById("layer").classList.replace('d-none','d-block');
 }
+removeAll.addEventListener("click",clearData);
 
 //  diplay diiv to choice for that if is the display block or none 
 function showDiv()
@@ -92,7 +148,9 @@ function remove()
 
 function addProduct()
 { 
-Product=
+if (validation(reguxNumber,ProductPrice)==true && validation(reguxeName,ProductName)==true && validation(reguxDes,ProductDesc)==true&& validation(reguxCatgorey,ProductCatgorey)==true)
+{
+let Product=
     {
         Name:ProductName.value,
         Price:ProductPrice.value,
@@ -105,19 +163,29 @@ Product=
     // add data in local storage
     displayData(store);
     makeInputsEmpty();
+    callreguex()
 }
+}
+btnAdd.addEventListener('click',addProduct)
+
+
 
 
 
 // function empty filds
-
 function makeInputsEmpty()
 {
     ProductName.value="";
     ProductPrice.value="";
     ProductCatgorey.value="";
     ProductDesc.value="";
+    callreguex()
 }
+btnClear.addEventListener("click",makeInputsEmpty)
+
+
+
+
 
 
 
@@ -167,11 +235,11 @@ function update(x)
     btnAdd.classList.add('d-none');
     btnUpdate.classList.replace('d-none','d-inline-block')
 
-
     inputsStyle(ProductName);
     inputsStyle(ProductDesc);
     inputsStyle(ProductPrice);
-    inputsStyle(ProductCatgorey);
+    inputsStyle(ProductCatgorey); 
+    callreguex();
 }
 
 // function stylest the fileds of inputes to know that is update
@@ -182,36 +250,37 @@ function inputsStyle(productInputs)
     productInputs.style.boxShadow="#596c6cb8 1px 0px 10px";
     productInputs.style.fontSize="16px";
 }
-
-
-
-
-
-
 function changeUpdate()
 {
-    if(ProductName.value!=store[numberchangeUpdate].Name)
+
+    if(validation(reguxeName,ProductName)==true)
     {
-        store[numberchangeUpdate].Name=ProductName.value;
+                if(ProductName.value!=store[numberchangeUpdate].Name)
+            {
+                store[numberchangeUpdate].Name=ProductName.value;
+            }
     }
-
-    if(ProductCatgorey.value!=store[numberchangeUpdate].Catgorey)
+    if(validation(reguxCatgorey,ProductCatgorey)==true)
     {
-        store[numberchangeUpdate].Catgorey==ProductCatgorey.value;
+         if(ProductCatgorey.value!=store[numberchangeUpdate].Catgorey)
+            {
+                store[numberchangeUpdate].Catgorey==ProductCatgorey.value;
+            }
     }
-
-
-    if(ProductPrice.value!=store[numberchangeUpdate].Price)
+    if(validation(reguxNumber,ProductPrice)==true)
     {
-     store[numberchangeUpdate].price==ProductPrice.value;
+            if(ProductPrice.value!=store[numberchangeUpdate].Price)
+                {
+                store[numberchangeUpdate].price==ProductPrice.value;
+                }
     }
-
-
-    if(ProductDesc.value!=store[numberchangeUpdate].Desc)
+    if(validation(reguxDes,ProductDesc)==true)
     {
-        store[numberchangeUpdate].Desc=ProductDesc.value;
+        if(ProductDesc.value!=store[numberchangeUpdate].Desc)
+        {
+            store[numberchangeUpdate].Desc=ProductDesc.value;
+        }
     }
-
     localStorage.setItem('ProductItem',JSON.stringify(store));
     makeInputsEmpty();
     btnAdd.classList.replace('d-none','d-inline-block')
@@ -222,8 +291,9 @@ function changeUpdate()
     removeInputsStyle(ProductDesc);
     removeInputsStyle(ProductPrice);
     removeInputsStyle(ProductCatgorey);
-
 }
+
+btnUpdate.addEventListener('click',changeUpdate);
 function removeInputsStyle(productInputs)
 {
     productInputs.style.backgroundColor="#fff";
@@ -239,7 +309,6 @@ function Search(termSearch)
     searchStore=[];
     if(searchInputs.value.length>2)
     {
-
         for (var s=0; s<store.length;s++)
         {
             if(store[s].Name.toLowerCase().includes(termSearch.toLowerCase()) == true)
@@ -253,6 +322,10 @@ else
 {
     searchStore=[];
     displayData(store)
+}
+}
+searchInputs.addEventListener("input",function(){
+    Search(this);
+})
 
-}
-}
+
